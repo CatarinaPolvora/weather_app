617f22fa5df901af5e79900ff98d2a1290384b66
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-06-03 01:10:56 +0100

    added forecast api

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ function showTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+ 
 }
 
 function showWeather(response) {
@@ -90,6 +91,7 @@ function showWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  
 }
 
 function showPosition(position) {
@@ -108,7 +110,8 @@ document
   .querySelector("#current-location")
   .addEventListener("click", getPosition);
 
-  function displayForecast() {
+  function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun","Mon"];
@@ -136,5 +139,13 @@ document
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+  getForecast(response.data.coord);
 }
-displayForecast();
+
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "d4a6a488c953cc85e69ba334cc42f424";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
